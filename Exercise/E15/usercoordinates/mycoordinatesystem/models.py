15130c6f35fa8147d86612f0908bc78ab62d50b2
FARAZ <0&1@DESKTOP-LQBSH9A.(none)>
2024-12-01 15:32:51 +0330

    add E15 file

---
+++ Exercise/E15/usercoordinates/mycoordinatesystem/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class UserLocation(models.Model):
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Lat: {self.latitude}, Lon: {self.longitude}"
+
+
